eabf950d3ccd52777013ddc2ac99502d29b7ae33
shcheglakov <Shcheglakov.ilya@mail.ru>
2025-11-04 13:16:47 +0300

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { Main } from './pages/main/main.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Main />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
