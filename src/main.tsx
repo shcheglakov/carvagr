f26db53809cf639078fde318cc3ef54cdd678bf1
shcheglakov <Shcheglakov.ilya@mail.ru>
2025-11-04 13:20:51 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
-import { HashRouter } from 'react-router'
+import { BrowserRouter } from 'react-router'
 import { Main } from './pages/main/main.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Main />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
