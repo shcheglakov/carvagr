64c2be0ef8cc614c79b0b70940710ef6a52d2956
shcheglakov <Shcheglakov.ilya@mail.ru>
2025-11-17 16:27:10 +0300

    17.11-2

--- src/widgets/components/services/aboutServices/aboutServices.tsx
+++ src/widgets/components/services/aboutServices/aboutServices.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+import styles from "./aboutServices.module.scss"
+
+interface AboutServicesProps {
+    status: number
+}
+
 export const AboutServices = () => {
+    const [isActiveService, setIsActiveService] = useState(0)
+    
+    const handleSwitchService = ({status}:AboutServicesProps) => {
+        setIsActiveService(status)
+    }
     return (
-        <div>
-            About Services
+        <div className={styles.container}>
+            <div className={styles.buttons_container}>
+                <button>Подбор</button>
+                <button>Осмотр</button>
+            </div>
+            {isActiveService === 0 ?
+                <div className={styles.text_block}>
+                    Подбор
+                </div>
+            :
+                <div className={styles.text_block}>
+                    Осмотр
+                </div>
+            }
+            
         </div>
     )
 }
